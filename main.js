7abc541cf02bd10e0bacc073cc3fed851ca44680
Steve <callasteven@gmail.com>
2021-02-18 09:06:09 -0700

    Add random function and randomize message

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //query selector variables go here 👇
-
+var meditationImage = document.querySelector('.meditation-image');
+var message = document.querySelector('.message');
 
 //global variables go here 👇
 var currentMessage;
@@ -12,11 +13,16 @@ var savedMessages = [];
 
 //functions and event handlers go here 👇
 function generateMessage() {
-  currentMessage = new Message(affirmations[0]);
+  currentMessage = new Message(affirmations[getRandomIndex(affirmations)]);
   return currentMessage;
 }
 
+function getRandomIndex(inputMessageList) {
+  return Math.floor(Math.random() * inputMessageList.length);
+}
+
+
 
 //testing goes here
-console.log(affirmations[0]);
-console.log(mantras[5]);
+// console.log(affirmations[0]);
+// console.log(mantras[5]);
